c80584e3b13a1c070aae3552ca0278d2b7bac4e4
wmeller <wesley.eller@gmail.com>
2020-04-07 18:03:37 -0700

    No typos, checked function returns.

--- SubnetGenerator.py
+++ SubnetGenerator.py
@@ -5,22 +5,28 @@ def GenerateSubnets(StartAddress, Size, ReqMatrix):
     #Given a start address, a subnet and a list of requirements, determine the start and end addresses of each of the resulting subnets as well as the address block in reserve.
     #Check that the inputs are correctly formatted
     r = re.compile('/\d*')
-    if r.match(Size) is None or int(Size[1:]) > 31: return "Check the size of the start subnet: should be format /xx between 1 and 31"
+    if r.match(Size) is None or int(Size[1:]) > 31:
+        print("Check the size of the start subnet: should be format /xx between 1 and 31")
+        return
     SubnetCIDR = int(Size[1:])
     r = re.compile('\d*.\d*.\d*.\d*')
-    if r.match(StartAddress) is None: return "check the format of the starting address: should be xxx.xxx.xxx.xxx"
+    if r.match(StartAddress) is None:
+        print("check the format of the starting address: should be xxx.xxx.xxx.xxx")
+        return
     #Get octets from start address
     StartAddressOcts = StartAddress.split('.')
-    StartAddressIP = ipaddress.ip_address(StartAddress)
     #Check that the address is valid
-    if any([int(x)>255 for x in StartAddressOcts]): return "Check starting address. No octet can be larger then 255."
+    if any([int(x)>255 for x in StartAddressOcts]):
+        print("Check starting address. No octet can be larger then 255.")
+        return
+    StartAddressIP = ipaddress.ip_address(StartAddress)
     #A list of the octets in binary for display
     StartAddressOcts = [bin(int(x))[2:].zfill(8) for x in StartAddressOcts]
-    print("Start Address Binary: "+".".join(StartAddressOcts[0]))
+    print("Start Address Binary: "+".".join(StartAddressOcts))
     #A list of the octets in binary for display
     BinSubnetMask = ''.zfill(32-SubnetCIDR).rjust(32, '1')
     print("Subnet Mask: "+BinSubnetMask)
-    #Loop through the 
+    #Loop through the
 
 
     return
