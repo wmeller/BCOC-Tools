4dbbc6bb133a289872ef1a852d3819727c512b80
Wesley Eller <wesleyeller@Wesleys-MacBook-Pro.local>
2020-04-23 22:49:24 -0700

    AJAX in. Calls once for every row. No idea why.

--- FlaskDevServer.py
+++ FlaskDevServer.py
@@ -16,13 +16,22 @@ ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '6d5sf4sa65f4as65f'
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=["GET",  "POST"])
 def main_page():
+    print('Running main page')
     if 'VLANList' not in session:
         session['VLANList'] = OrderedDict({'1':{'ID':1, 'Name':'Servers'}, '2':{'ID':2, 'Name':'Users'}})
         session.modified = True
     return render_template('VTPBuilder.html', SessionData=session)
 
-@app.route('/add_vlan', methods=["GET"])
+@app.route('/add_vlan', methods=["GET", "POST"])
 def add_vlan():
-    session['VLANList'].append
+    #This is getting called a whole bunch of times for every click right now.
+    print('Running add vlan script')
+    LastID = list(session['VLANList'].keys())[-1]
+    NextID = str(int(LastID)+1)
+    session['VLANList'][NextID]={'ID':int(NextID), 'Name':""}
+    session.modified = True
+    print('Printing VLAN list...')
+    print(session['VLANList'])
+    return redirect(url_for('add_vlan', _anchor="end_VLAN_table"))
